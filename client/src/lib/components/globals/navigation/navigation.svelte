5a182b87362860f6d33c086fa4736894a6e42171
Sam O'Donnell (Oh-Dee) <sodonnell5214@gmail.com>
2024-02-27 17:21:12 -0500

    Added styling to homepage.

--- client/src/lib/components/globals/navigation/navigation.svelte
+++ client/src/lib/components/globals/navigation/navigation.svelte
@@ -1,35 +1,52 @@
 <script>
-    import {page} from '$app/stores';
-    import logo from '$lib/images/logo/alt_logo.svg';
-    import {Dropdown, DropdownItem, Navbar, NavBrand, NavHamburger, NavLi, NavUl} from 'flowbite-svelte';
-    import {ChevronDownOutline} from 'flowbite-svelte-icons';
+	import { page } from '$app/stores';
+	import logo from '$lib/images/logo/alt_logo.svg';
+	import {
+		Dropdown,
+		DropdownItem,
+		Navbar,
+		NavBrand,
+		NavHamburger,
+		NavLi,
+		NavUl
+	} from 'flowbite-svelte';
+	import { ChevronDownOutline } from 'flowbite-svelte-icons';
 
-    $: activeUrl = $page.url.pathname;
-    export let navItems;
+	$: activeUrl = $page.url.pathname;
+	export let navItems;
 </script>
 
 <Navbar let:hidden let:toggle class="bg-transparent">
-    <NavBrand href="/">
-        <img src={logo} class="me-3 w-40 text-myrtle_green-600 logo" alt="Catholic Momma Mouse"/>
-        <span class="hidden self-center whitespace-nowrap text-xl font-semibold"
-        >Catholic Momma Mouse</span
-        >
-    </NavBrand>
-    <NavHamburger on:click={toggle}/>
-    <NavUl {hidden} {activeUrl}>
-        <NavLi class="cursor-pointer text-myrtle_green-600 md:hover:text-myrtle_green-800">
-            Dropdown
-            <ChevronDownOutline class="w-3 h-3 ms-2 text-myrtle_green-600 md:hover:text-myrtle_green-800 inline"/>
-        </NavLi>
-        <Dropdown containerClass="bg-transparent " class="w-44 z-20 text-myrtle_green-600">
-            <DropdownItem class="md:hover:text-myrtle_green-800 hover:bg-transparent" href="/">Dashboard</DropdownItem>
-            <DropdownItem class="md:hover:text-myrtle_green-800 hover:bg-transparent" href="/docs/components/navbar">
-                Settings
-            </DropdownItem>
-            <DropdownItem class="md:hover:text-myrtle_green-800 hover:bg-transparent" href="/">Earnings</DropdownItem>
-        </Dropdown>
-        {#each navItems as {title, href}}
-            <NavLi class="text-myrtle_green-600 md:hover:text-myrtle_green-800" {href}>{title}</NavLi>
-        {/each}
-    </NavUl>
+	<NavBrand href="/">
+		<img src={logo} class="me-3 w-40 bg-secondary-600 logo" alt="Catholic Momma Mouse" />
+		<span class="hidden self-center whitespace-nowrap text-xl font-semibold"
+			>Catholic Momma Mouse</span
+		>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden} {activeUrl}>
+		<NavLi class="cursor-pointer text-secondary-600 md:hover:text-secondary-800">
+			Reviews
+			<ChevronDownOutline
+				class="w-3 h-3 ms-2 text-secondary-600 md:hover:text-secondary-800 inline"
+			/>
+		</NavLi>
+		<Dropdown containerClass="bg-transparent " class="w-44 z-20 text-secondary-600">
+			<DropdownItem class="md:hover:text-secondary-800 hover:bg-transparent" href="/"
+				>Dashboard
+			</DropdownItem>
+			<DropdownItem
+				class="md:hover:text-secondary-800 hover:bg-transparent"
+				href="/docs/components/navbar"
+			>
+				Settings
+			</DropdownItem>
+			<DropdownItem class="md:hover:text-secondary-800 hover:bg-transparent" href="/"
+				>Earnings
+			</DropdownItem>
+		</Dropdown>
+		{#each navItems as { title, href }}
+			<NavLi class="text-secondary-600 md:hover:text-secondary-800" {href}>{title}</NavLi>
+		{/each}
+	</NavUl>
 </Navbar>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,84 +1,102 @@
+const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
-	],
-	plugins: [require('flowbite/plugin')],
-	darkMode: 'class',
-	theme: {
-		fontFamily: {
-			montserrat: ['Montserrat', 'sans-serif']
-		},
-		extend: {
-			colors: {
-				primary: {
-					DEFAULT: '#00233c',
-					100: '#00070c',
-					200: '#000e18',
-					300: '#001525',
-					400: '#001d31',
-					500: '#00233c',
-					600: '#005897',
-					700: '#008cf1',
-					800: '#4bb4ff',
-					900: '#a5daff'
-				},
-				secondary: {
-					DEFAULT: '#48beff',
-					100: '#002a41',
-					200: '#005583',
-					300: '#007fc4',
-					400: '#06a8ff',
-					500: '#48beff',
-					600: '#6cccff',
-					700: '#91d8ff',
-					800: '#b6e5ff',
-					900: '#daf2ff'
-				},
-				tertiary: {
-					DEFAULT: '#e5d1d0',
-					100: '#39201f',
-					200: '#71413e',
-					300: '#a76360',
-					400: '#c79b98',
-					500: '#e5d1d0',
-					600: '#ebdbda',
-					700: '#f0e4e3',
-					800: '#f5eded',
-					900: '#faf6f6'
-				},
-				yellow_green: {
-					DEFAULT: '#99c133',
-					100: '#1f270a',
-					200: '#3d4d15',
-					300: '#5c741f',
-					400: '#7b9b29',
-					500: '#99c133',
-					600: '#b0d257',
-					700: '#c3de81',
-					800: '#d7e9ab',
-					900: '#ebf4d5'
-				},
-				myrtle_green: {
-					DEFAULT: '#3d7068',
-					100: '#0c1615',
-					200: '#192d2a',
-					300: '#25433f',
-					400: '#315953',
-					500: '#3d7068',
-					600: '#559b91',
-					700: '#7db7af',
-					800: '#a8cfc9',
-					900: '#d4e7e4'
-				},
-				white: {
-					DEFAULT: '#eee'
-				},
-				black: {
-					DEFAULT: '#333'
-				}
-			}
-		}
-	}
+    content: [
+        './src/**/*.{html,js,svelte,ts}',
+        './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+    ],
+    plugins: [
+        require('flowbite/plugin'),
+        plugin(function ({matchUtilities, theme}) {
+            matchUtilities(
+                {
+                    'text-shadow-white': (value) => ({
+                        textShadow: value
+                    })
+                },
+                {values: theme('textShadowWhite')}
+            );
+        })
+    ],
+    darkMode: 'class',
+    theme: {
+        fontFamily: {
+            montserrat: ['Montserrat', 'sans-serif']
+        },
+        extend: {
+            textShadowWhite: {
+                sm: '0 1px 2px #eee',
+                DEFAULT: '1px 2px #eee',
+                lg: '0 8px 16px #eee'
+            },
+            colors: {
+                primary: {
+                    DEFAULT: '#00233c',
+                    100: '#00070c',
+                    200: '#000e18',
+                    300: '#001525',
+                    400: '#001d31',
+                    500: '#00233c',
+                    600: '#005897',
+                    700: '#008cf1',
+                    800: '#4bb4ff',
+                    900: '#a5daff'
+                },
+                secondary: {
+                    DEFAULT: '#48beff',
+                    100: '#002a41',
+                    200: '#005583',
+                    300: '#007fc4',
+                    400: '#06a8ff',
+                    500: '#48beff',
+                    600: '#6cccff',
+                    700: '#91d8ff',
+                    800: '#b6e5ff',
+                    900: '#daf2ff'
+                },
+                tertiary: {
+                    DEFAULT: '#e5d1d0',
+                    100: '#39201f',
+                    200: '#71413e',
+                    300: '#a76360',
+                    400: '#c79b98',
+                    500: '#e5d1d0',
+                    600: '#ebdbda',
+                    700: '#f0e4e3',
+                    800: '#f5eded',
+                    900: '#faf6f6'
+                },
+                yellow_green: {
+                    DEFAULT: '#99c133',
+                    100: '#1f270a',
+                    200: '#3d4d15',
+                    300: '#5c741f',
+                    400: '#7b9b29',
+                    500: '#99c133',
+                    600: '#b0d257',
+                    700: '#c3de81',
+                    800: '#d7e9ab',
+                    900: '#ebf4d5'
+                },
+                myrtle_green: {
+                    DEFAULT: '#3d7068',
+                    100: '#0c1615',
+                    200: '#192d2a',
+                    300: '#25433f',
+                    400: '#315953',
+                    500: '#3d7068',
+                    600: '#559b91',
+                    700: '#7db7af',
+                    800: '#a8cfc9',
+                    900: '#d4e7e4'
+                },
+                white: {
+                    DEFAULT: '#eee'
+                },
+                black: {
+                    DEFAULT: '#333'
+                }
+            }
+        }
+    }
 };
