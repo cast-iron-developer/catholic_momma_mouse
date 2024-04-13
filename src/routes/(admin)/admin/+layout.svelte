c8fe07a0aebfd57a2d973a360172fc5db923c5f4
Sam O'Donnell (Oh-Dee) <sodonnell5214@gmail.com>
2024-04-13 15:05:58 -0400

    Adding basic logic for admin console.

--- src/routes/(admin)/admin/+layout.svelte
+++ src/routes/(admin)/admin/+layout.svelte
@@ -1,1 +1,15 @@
-<slot />
+<script>
+	import '../../../app.scss';
+</script>
+
+<svelte:head>
+	<title>Catholic Momma Mouse</title>
+	<meta
+		name="description"
+		content="This is the culmination of many years worth of dreaming and a little bit of hard work. Please enjoy a Catholic Disney Blog made by a mother who loves both her Catholic Faith and Disney World."
+	/>
+</svelte:head>
+
+<main class="font-montserrat">
+	<slot />
+</main>
